4d9716ce2c4fb7ab422d503df23ee464510e6d26
Yasser Nadeem <ok.yasser@gmail.com>
2022-01-12 05:06:41 -0500

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPT723jx32obVL5mIIWZxaAc2_Njcm2NQ",
+  authDomain: "hpp-create-prototype.firebaseapp.com",
+  projectId: "hpp-create-prototype",
+  storageBucket: "hpp-create-prototype.appspot.com",
+  messagingSenderId: "399399692650",
+  appId: "1:399399692650:web:b49a31699fef781e525d13"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
